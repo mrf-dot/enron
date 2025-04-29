4d9bd9c7ed2c433d42fd4da0d0b6654affb7e146
mrf-dot <mitch@mitchf.me>
2025-04-29 13:03:14 -0400

    added .awk to file extension

--- get-phone-numbers.sh
+++ get-phone-numbers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 lines=$(wc -l < emails.csv)
 echo "Filtering the email CSV file for phone numbers... (operating on $lines lines)"
-awk -f  extract-phone-numbers emails.csv | sort -nr > phone-numbers.txt
+awk -f extract-phone-numbers.awk emails.csv | sort -nr > phone-numbers.txt
 lines=$(wc -l < phone-numbers.txt)
 echo "Computing the frequencies... (operating on $lines lines)"
 awk -f area-code-ranks.awk phone-numbers.txt | sort -nr > ranked-area-codes.txt
